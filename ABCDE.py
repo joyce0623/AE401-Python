264cfe143f80c4ab25516c560e4fd46b03a88645
joyce0623 <78334488+joyce0623@users.noreply.github.com>
2021-02-01 17:09:13 +0800

    Add files via upload

---
+++ ABCDE.py
@@ -0,0 +1,12 @@
+score=input('請輸入成績')
+score=int(score)
+if score>=90:
+ print('A')
+elif score>=80:
+ print('B')
+elif score>=70:
+ print('C')
+elif score>=60:
+ print('D')
+elif score>=0:
+ print('E')
